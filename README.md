e6c32a0f3b9129d0f0e7cdc2b31458772e37e50a
attTuo <tuomaalanatte@gmail.com>
2025-03-13 14:26:45 +0200

    project start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # numbered_cards
+
+*Create a deck with a specified amount of numbered cards. After creation the deck gets shuffled and you can then draw cards from the deck*
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <h1 className="text-4xl">Start</h1>
+      <p>Hello</p>
+    </div>
+  );
+}
