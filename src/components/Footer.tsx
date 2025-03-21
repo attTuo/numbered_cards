375ea7bdfb6c2ebcc6116e5776ebc2ca22863c10
attTuo <tuomaalanatte@gmail.com>
2025-03-21 16:33:19 +0200

    add - basic header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return(
+    <div className="flex justify-center w-full max-w-[1440px] bg-primary-background p-4 items-center">
+      <h1 className="text-3xl">Numbered cards - 2025</h1>
+    </div>
+  )
+}
